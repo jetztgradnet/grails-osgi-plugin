77cc1fc476bad34c133423a8ee3d773d8b578f98
Wolfgang Schell <github@jetztgrad.net>
2010-07-08 02:14:20 +0200

    split module configuration

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -13,34 +13,103 @@ def springDMImportSpecs = "version=\"[2.0.0, 3.0.0)\";$optional"
 
 def springVersion = '3.0.3.RELEASE'
 
-def libToBundleImport = [
-    'activation-1.1.jar':  'package:javax.activation',// 'package:javax.activation;version="1.1.1"',
-    'activation.jar': '', //'package:javax.activation',
-    'antlr-2.7.6.jar': 'bundle:com.springsource.antlr;version="[2.7.7,2.7.7]"',
-    'aopalliance-1.0.jar': 'package:org.aopalliance.aop;version="1.0.0",org.aopalliance.intercept;version="1.0.0"', //bundle:com.springsource.org.aopalliance;version="[3.0.0.RELEASE, 4.0.0)"
-    'aspectjrt-1.6.6.jar': 'bundle:com.springsource.org.aspectj.runtime;version="1.6.6.RELEASE"',
-    'aspectjweaver-1.6.6.jar': 'bundle:com.springsource.org.aspectj.runtime;version="1.6.6.RELEASE"',
+def commonModulesConfig = [
+	// AspectJ, byte code generation, and weaving support
+	'antlr-2.7.6.jar': 'bundle:com.springsource.antlr;version="[2.7.7,2.7.7]"',
+	'aopalliance-1.0.jar': 'package:org.aopalliance.aop;version="1.0.0",org.aopalliance.intercept;version="1.0.0"', //bundle:com.springsource.org.aopalliance;version="[3.0.0.RELEASE, 4.0.0)"
+	'aspectjrt-1.6.6.jar': 'bundle:com.springsource.org.aspectj.runtime;version="1.6.6.RELEASE"',
+	'aspectjweaver-1.6.6.jar': 'bundle:com.springsource.org.aspectj.runtime;version="1.6.6.RELEASE"',
 	'aspectjrt-1.6.8.jar': 'bundle:com.springsource.org.aspectj.runtime;version="1.6.8.RELEASE"',
-    'aspectjweaver-1.6.8.jar': 'bundle:com.springsource.org.aspectj.runtime;version="1.6.8.RELEASE"',
-    'catalina-ant.jar': '',
-    'cglib-nodep-2.1_3.jar': 'bundle:com.springsource.net.sf.cglib;version="[2.1.3,2.2.0)"',
-    'commons-beanutils-1.8.0.jar': 'bundle:com.springsource.org.apache.commons.beanutils;version="1.8.0"',
-    'commons-codec-1.3.jar': 'bundle:com.springsource.org.apache.commons.codec;version="1.3.0"',
-    'commons-collections-3.2.1.jar': 'bundle:com.springsource.org.apache.commons.collections;version="3.2.1"',
-    'commons-dbcp-1.2.2.jar': 'bundle:com.springsource.org.apache.commons.dbcp;version="1.2.2.osgi"',
-    'commons-el-1.0.jar': 'bundle:com.springsource.org.apache.commons.el;version="1.0.0"',
-    'commons-fileupload-1.2.1.jar': 'bundle:com.springsource.org.apache.commons.fileupload;version="1.2.1"',
-    'commons-httpclient-3.1.jar': 'bundle:com.springsource.org.apache.commons.httpclient;version="3.1.0"',
-    'commons-io-1.4.jar': 'bundle:com.springsource.org.apache.commons.io;version="1.4.0"',
-    'commons-lang-2.4.jar': 'bundle:com.springsource.org.apache.commons.lang;version="2.4.0"',
-    'commons-pool-1.5.3.jar': 'bundle:com.springsource.org.apache.commons.pool;version="1.5.3"',
-    'commons-validator-1.3.1.jar': 'bundle:com.springsource.org.apache.commons.validator;version="1.3.1"',
-    'dom4j-1.6.1.jar': 'bundle:com.springsource.org.dom4j;version="1.6.1"',
-    //'ehcache-core-1.7.1.jar': '',
-    'ejb3-persistence-1.0.2.GA.jar': 'bundle:com.springsource.javax.persistence;version="[1.0.0,2.0.0)"',
-    //'facebook-java-api-2.0.4.jar': '',
-    //'facebook-java-api-schema-2.0.4.jar': '',
-//	'grails-bootstrap-1.2.0.jar': '',
+	'aspectjweaver-1.6.8.jar': 'bundle:com.springsource.org.aspectj.runtime;version="1.6.8.RELEASE"',
+	'cglib-nodep-2.1_3.jar': 'bundle:com.springsource.net.sf.cglib;version="[2.1.3,2.2.0)"',
+	'javassist-3.8.0.GA.jar': 'bundle:com.springsource.javassist;version="[3.9.0, 4.0.0)"',
+	'javassist-3.11.0.GA.jar': 'bundle:com.springsource.javassist;version="[3.9.0, 4.0.0)"',
+
+	// Apache Jakarta Commons
+	'commons-beanutils-1.8.0.jar': 'bundle:com.springsource.org.apache.commons.beanutils;version="1.8.0"',
+	'commons-codec-1.3.jar': 'bundle:com.springsource.org.apache.commons.codec;version="1.3.0"',
+	'commons-collections-3.2.1.jar': 'bundle:com.springsource.org.apache.commons.collections;version="3.2.1"',
+	'commons-dbcp-1.2.2.jar': 'bundle:com.springsource.org.apache.commons.dbcp;version="1.2.2.osgi"',
+	'commons-el-1.0.jar': 'bundle:com.springsource.org.apache.commons.el;version="1.0.0"',
+	'commons-fileupload-1.2.1.jar': 'bundle:com.springsource.org.apache.commons.fileupload;version="1.2.1"',
+	'commons-httpclient-3.1.jar': 'bundle:com.springsource.org.apache.commons.httpclient;version="3.1.0"',
+	'commons-io-1.4.jar': 'bundle:com.springsource.org.apache.commons.io;version="1.4.0"',
+	'commons-lang-2.4.jar': 'bundle:com.springsource.org.apache.commons.lang;version="2.4.0"',
+	'commons-pool-1.5.3.jar': 'bundle:com.springsource.org.apache.commons.pool;version="1.5.3"',
+	'commons-validator-1.3.1.jar': 'bundle:com.springsource.org.apache.commons.validator;version="1.3.1"',
+
+	// XML and XSL(T)
+	'xalan-2.7.1.jar': '',
+	'xercesImpl-2.9.1.jar': 'bundle:com.springsource.org.apache.xmlcommons;version="[1.3.4,1.4.0)"', // Xerces is a fragment and attaches to xmlxcommons 'bundle:com.springsource.org.apache.xerces;version="[2.9.1,3.0.0)"',
+	'xmlsec-1.4.2.jar': '',
+	'xpp3_min-1.1.3.4.O.jar': '',
+	'dom4j-1.6.1.jar': 'bundle:com.springsource.org.dom4j;version="1.6.1"',
+
+	// logging related
+	'jcl-over-slf4j-1.5.8.jar': 'package:org.apache.commons.logging;version="[1.1.1, 1.2.0)"',
+	'jul-to-slf4j-1.5.8.jar': '',
+	'log4j-1.2.15.jar': '',
+	'slf4j-api-1.5.8.jar': 'package:org.slf4j;version="[1.5.6, 1.6.0)"',
+	'slf4j-log4j12-1.5.8.jar': '',
+
+	// standard Java SE/EE apis
+	'jta-1.1.jar': 'package:javax.transaction',
+	'jaxb-api-2.1.jar': '',
+	'jaxb-impl-2.1.3.jar': '',
+	'stax-api-1.0-2.jar': '',
+	'mail.jar': 'bundle:com.springsource.javax.mail;version="1.4.1"',
+	'activation-1.1.jar':  'package:javax.activation',// 'package:javax.activation;version="1.1.1"',
+	'activation.jar': '', //'package:javax.activation',
+
+	// web related
+	'standard-1.1.2.jar': '', //'bundle:com.springsource.javax.servlet.jsp.jstl;version="[1.1.0, 2.0.0)"',
+	'jstl-1.1.2.jar': '',
+
+	// misc
+	'oro-2.0.8.jar': 'bundle:com.springsource.org.apache.oro;version="[2.0.8, 2.1.0)"',
+	'ivy-2.1.0.jar': 'bundle:com.springsource.org.apache.ivy;version="[2.0, 2.2.0)"',
+]
+
+def springModulesConfig = [
+	("org.springframework.aop-${springVersion}.jar".toString()) : 'bundle:org.springframework.aop;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.asm-${springVersion}.jar".toString()) : 'bundle:org.springframework.asm;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.aspects-${springVersion}.jar".toString()) : 'bundle:org.springframework.aspects;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.beans-${springVersion}.jar".toString()) : 'bundle:org.springframework.beans;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.context-${springVersion}.jar".toString()) : 'bundle:org.springframework.context;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.context.support-${springVersion}.jar".toString()) : 'bundle:org.springframework.context.support;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.core-${springVersion}.jar".toString()) : 'bundle:org.springframework.core;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.expression-${springVersion}.jar".toString()) : 'bundle:org.springframework.expression;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.instrument-${springVersion}.jar".toString()) : 'bundle:org.springframework.instrument;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.jdbc-${springVersion}.jar".toString()) : 'bundle:org.springframework.jdbc;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.jms-${springVersion}.jar".toString()) : 'bundle:org.springframework.jms;version="[3.0.0.RELEASE, 4.0.0)"',
+//	("org.springframework.orm-${springVersion}.jar".toString()) : 'bundle:org.springframework.orm;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.oxm-${springVersion}.jar".toString()) : 'bundle:org.springframework.oxm;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.transaction-${springVersion}.jar".toString()) : 'bundle:org.springframework.transaction;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.web-${springVersion}.jar".toString()) : 'bundle:org.springframework.web;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.web.servlet-${springVersion}.jar".toString()) : 'bundle:org.springframework.web.servlet;version="[3.0.0.RELEASE, 4.0.0)"',
+	("org.springframework.web.portlet-${springVersion}.jar".toString()) : 'bundle:org.springframework.web.portlet;version="[3.0.0.RELEASE, 4.0.0)"',
+    
+]
+
+def persistenceModulesConfig = [
+	'hibernate-annotations-3.4.0.GA.jar': '', // this is a fragment to the core hibernate module... 'bundle:com.springsource.org.hibernate.annotations;version="[3.4.0,3.5.0)"',
+	'hibernate-commons-annotations-3.3.0.ga.jar': 'bundle:com.springsource.org.hibernate.annotations.common;version="[3.3.0,3.4.0)"',
+	'hibernate-commons-annotations-3.1.0.GA.jar': 'bundle:com.springsource.org.hibernate.annotations.common;version="[3.3.0,3.4.0)"',
+	'hibernate-core-3.3.1.GA.jar': 'bundle:com.springsource.org.hibernate;version="[3.3.1,3.4.0)"',
+	'hibernate-ehcache-3.3.1.GA.jar': '', // this is a fragment to the core hibernate module... 'bundle:com.springsource.org.hibernate.cache;version="[3.3.0,3.5.0)",com.springsource.net.sf.ehcache;version="[1.6.2,1.8.0)"',
+
+	("org.springframework.orm-${springVersion}.jar".toString()) : 'bundle:org.springframework.orm;version="[3.0.0.RELEASE, 4.0.0)"',
+
+	'ejb3-persistence-1.0.2.GA.jar': 'bundle:com.springsource.javax.persistence;version="[1.0.0,2.0.0)"',
+]
+
+def groovyModulesConfig = [
+	'groovy-all-1.6.7.jar': 'bundle:groovy-all;version="1.6.7"',
+	'groovy-all-1.7.3.jar': 'bundle:groovy-all;version="1.7.3"',
+]
+
+def grailsModulesConfig = [
+	'grails-bootstrap-1.2.0.jar': '',
 //	'grails-core-1.2.0.jar': 'bundle:org.grails.osgi;version="[1.2.0,2.0.0)"', // bundle contains core and bootstrap
 //	'grails-crud-1.2.0.jar': 'bundle:org.grails.crud;version="[1.2.0,2.0.0)"',
 ////	'grails-docs-1.2.0.jar': 'bundle:org.grails.docs;version="[1.2.0,2.0.0)"',
@@ -49,7 +118,7 @@ def libToBundleImport = [
 //	'grails-spring-1.2.0.jar': 'bundle:org.grails.spring;version="[1.2.0,2.0.0)"',
 //	'grails-web-1.2.0.jar': 'bundle:org.grails.web;version="[1.2.0,2.0.0)"',
 	
-//	'grails-bootstrap-1.2.1.jar': '',
+	'grails-bootstrap-1.2.1.jar': '',
 //	'grails-core-1.2.1.jar': 'bundle:org.grails.osgi;version="[1.2.0,2.0.0)"', // bundle contains core and bootstrap
 //	'grails-crud-1.2.1.jar': 'bundle:org.grails.crud;version="[1.2.0,2.0.0)"',
 ////	'grails-docs-1.2.1.jar': 'bundle:org.grails.docs;version="[1.2.0,2.0.0)"',
@@ -57,71 +126,58 @@ def libToBundleImport = [
 //	'grails-resources-1.2.1.jar': 'bundle:org.grails.resources;version="[1.2.0,2.0.0)"',
 //	'grails-spring-1.2.1.jar': 'bundle:org.grails.spring;version="[1.2.0,2.0.0)"',
 //	'grails-web-1.2.1.jar': 'bundle:org.grails.web;version="[1.2.0,2.0.0)"',
-	
-    'groovy-all-1.6.7.jar': 'bundle:groovy-all;version="1.6.7"',
-	'groovy-all-1.7.3.jar': 'bundle:groovy-all;version="1.7.3"',
-	
-    'hibernate-annotations-3.4.0.GA.jar': '', // this is a fragment to the core hibernate module... 'bundle:com.springsource.org.hibernate.annotations;version="[3.4.0,3.5.0)"',
-    'hibernate-commons-annotations-3.3.0.ga.jar': 'bundle:com.springsource.org.hibernate.annotations.common;version="[3.3.0,3.4.0)"',
-	'hibernate-commons-annotations-3.1.0.GA.jar': 'bundle:com.springsource.org.hibernate.annotations.common;version="[3.3.0,3.4.0)"',
-    'hibernate-core-3.3.1.GA.jar': 'bundle:com.springsource.org.hibernate;version="[3.3.1,3.4.0)"',
-    'hibernate-ehcache-3.3.1.GA.jar': '', // this is a fragment to the core hibernate module... 'bundle:com.springsource.org.hibernate.cache;version="[3.3.0,3.5.0)",com.springsource.net.sf.ehcache;version="[1.6.2,1.8.0)"',
-	
+]
+
+def miscGrailsModulesConfig = [
+    //'ehcache-core-1.7.1.jar': '',
+    
+    //'facebook-java-api-2.0.4.jar': '',
+    //'facebook-java-api-schema-2.0.4.jar': '',
+
     //'hsqldb-1.8.0.10.jar': '',
     //'htmlparser-1.6.jar': '',
     //'icu4j-3.4.4.jar': '',
-    'jasper-jdt.jar': '',
     //'java-openid-sxip-0.9.4-rebuilt.jar': '',
-    'javassist-3.8.0.GA.jar': 'bundle:com.springsource.javassist;version="[3.9.0, 4.0.0)"',
-	'javassist-3.11.0.GA.jar': 'bundle:com.springsource.javassist;version="[3.9.0, 4.0.0)"',
-    'jaxb-api-2.1.jar': '',
-    'jaxb-impl-2.1.3.jar': '',
-    'jcl-over-slf4j-1.5.8.jar': 'package:org.apache.commons.logging;version="[1.1.1, 1.2.0)"',
     //'json-20070829.jar': '',
-    'jta-1.1.jar': '',
+
     //'jug-1.1.2.jar': '',
-    'jul-to-slf4j-1.5.8.jar': '',
-    'log4j-1.2.15.jar': '',
-    'mail.jar': 'bundle:com.springsource.javax.mail;version="1.4.1"',
     //'nekohtml-1.9.12.jar': '',
     //'openxri-client-1.0.1.jar': '',
     //'openxri-syntax-1.0.1.jar': '',
-    ("org.springframework.aop-${springVersion}.jar".toString()) : 'bundle:org.springframework.aop;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.asm-${springVersion}.jar".toString()) : 'bundle:org.springframework.asm;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.aspects-${springVersion}.jar".toString()) : 'bundle:org.springframework.aspects;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.beans-${springVersion}.jar".toString()) : 'bundle:org.springframework.beans;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.context-${springVersion}.jar".toString()) : 'bundle:org.springframework.context;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.context.support-${springVersion}.jar".toString()) : 'bundle:org.springframework.context.support;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.core-${springVersion}.jar".toString()) : 'bundle:org.springframework.core;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.expression-${springVersion}.jar".toString()) : 'bundle:org.springframework.expression;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.instrument-${springVersion}.jar".toString()) : 'bundle:org.springframework.instrument;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.jdbc-${springVersion}.jar".toString()) : 'bundle:org.springframework.jdbc;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.jms-${springVersion}.jar".toString()) : 'bundle:org.springframework.jms;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.orm-${springVersion}.jar".toString()) : 'bundle:org.springframework.orm;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.oxm-${springVersion}.jar".toString()) : 'bundle:org.springframework.oxm;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.transaction-${springVersion}.jar".toString()) : 'bundle:org.springframework.transaction;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.web-${springVersion}.jar".toString()) : 'bundle:org.springframework.web;version="[3.0.0.RELEASE, 4.0.0)"',
-    ("org.springframework.web.servlet-${springVersion}.jar".toString()) : 'bundle:org.springframework.web.servlet;version="[3.0.0.RELEASE, 4.0.0)"',
-	("org.springframework.web.portlet-${springVersion}.jar".toString()) : 'bundle:org.springframework.web.portlet;version="[3.0.0.RELEASE, 4.0.0)"',
-	
-    'oro-2.0.8.jar': 'bundle:com.springsource.org.apache.oro;version="[2.0.8, 2.1.0)"',
+
 	//'recaptcha4j-0.0.7.jar': '',
 	//'shiro-all-1.0-incubating-SNAPSHOT.jar': '',
     'sitemesh-2.4.jar': 'bundle:com.springsource.com.opensymphony.sitemesh;version="[2.4.0, 2.5.0)"',
-    'slf4j-api-1.5.8.jar': 'package:org.slf4j;version="[1.5.6, 1.6.0)"',
-    'slf4j-log4j12-1.5.8.jar': '',
-    'standard-1.1.2.jar': '', //'bundle:com.springsource.javax.servlet.jsp.jstl;version="[1.1.0, 2.0.0)"',
-    'jstl-1.1.2.jar': '',
-    'stax-api-1.0-2.jar': '',
+]
+
+def blockedModulesConfig = [
+	// Equinox and OSGi framework
+	'org.eclipse.osgi-3.6.0.v20100517.jar': '',
+	'org.osgi.compendium-4.2.0.jar': '',
+
+	// tools (bnd, PAX)
+	'bndlib-0.0.357.jar': '',
+	'pax-swissbox-bnd-1.2.0.jar': '',
+	'pax-swissbox-optional-jcl-1.2.0.jar': '',
+	'pax-swissbox-tinybundles-1.2.0.jar': '',
+	'ops4j-base-io-1.2.1.jar': '',
+	'ops4j-base-lang-1.2.1.jar': '',
+	'ops4j-base-store-1.2.1.jar': '',
+
+	// Spring DM
+	'spring-osgi-core-2.0.0.M1.jar': '',
+	'spring-osgi-io-2.0.0.M1.jar': '',
+]
+
+def tomcatModulesConfig = [
+	// Tomcat
+	'catalina-ant.jar': '',
+	'jasper-jdt.jar': '',
 	'tomcat-core.jar': '',
 	'tomcat-dbcp.jar': '',
 	'tomcat-jasper.jar': '',
 	'tomcat-juli-adapters.jar': '',
 	'tomcat-juli.jar': '',
-    'xalan-2.7.1.jar': '',
-    'xercesImpl-2.9.1.jar': 'bundle:com.springsource.org.apache.xmlcommons;version="[1.3.4,1.4.0)"', // Xerces is a fragment and attaches to xmlxcommons 'bundle:com.springsource.org.apache.xerces;version="[2.9.1,3.0.0)"',
-    'xmlsec-1.4.2.jar': '',
-    'xpp3_min-1.1.3.4.O.jar': '',
 ]
 
 def packagingMode = BuildSettingsHolder.getSettings()?.config?.grails?.plugins?.osgi?.packagingMode
@@ -131,22 +187,39 @@ if (!packagingMode) {
 
 eventCreateWarStart = { warName, stagingDir -> 
 	def moduleConfig = [:]
+	
+	
+	
 
 	switch (packagingMode) {
 	case 'modular':
 		// replace some libs by importing bundles or packages
-		moduleConfig = libToBundleImport
-		break;
+		moduleConfig << blockedModulesConfig
+		moduleConfig << tomcatModulesConfig
+		moduleConfig << commonModulesConfig
+		moduleConfig << springModulesConfig
+		moduleConfig << persistenceModulesConfig
+		moduleConfig << groovyModulesConfig
+		moduleConfig << grailsModulesConfig
+		moduleConfig << miscGrailsModulesConfig
+		break
+	case 'basic':
+		// we put everything but Spring and its dependencies into the bundled war file
+		moduleConfig << blockedModulesConfig
+		moduleConfig << tomcatModulesConfig
+		moduleConfig << commonModulesConfig
+		moduleConfig << springModulesConfig
+		break
 	case 'monolithic':
 		// we put everything into the bundled war file
-		moduleConfig = [:]
-		break;
+		moduleConfig << blockedModulesConfig
+		break
 	default:
 		println "unknown packaging mode: $packagingMode"
-		break;
+		break
 	}
 
-	println "packaging as $packagingMode bundle"
+	println "packaging application $grailsAppName as $packagingMode bundle"
 
 	String metaInfo = "$stagingDir/META-INF" 
 	ant.mkdir(dir:metaInfo) 
